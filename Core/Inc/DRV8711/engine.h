626642eb123bc9788ef575d1f521934df03f3b94
Kordian Czyżewski <235768@edu.p.lodz.pl>
2024-11-16 03:31:42 -0800

    engine, error - inital ideas

--- Core/Inc/DRV8711/engine.h
+++ Core/Inc/DRV8711/engine.h
@@ -13,17 +13,46 @@ inline uint16_t getStepSize(uint16_t i){
     uint16_t result = (1U << i); 
     return (result > 256U)? 256U: result;
 }
+typedef enum
+{
+  Slow                = 0b000,
+  SlowIncMixedDec     = 0b001,
+  Fast                = 0b010,
+  Mixed               = 0b011,
+  SlowIncAutoMixedDec = 0b100,
+  AutoMixed           = 0b101,
+}DRV8711_DECAY_MODE_t;
+
 typedef struct{
     float angle;
     float velocity;
-#pragma region FLAGS /**/
-    unsigned int direction: 1;
-    unsigned int engine_enabled: 1;
-#pragma endregion FLAGS /**/
     DRV8711_ERROR_t error;
+    DRV8711_DECAY_MODE_t decay;
+    uint32_t ctrl, torque, off, blank, decay, stall, drive;
 
 
 }DRV8711_ENGINE_t;
+void applySettings(DRV8711_ENGINE_t);
+/*_____________DEFAULT_ENGINE_SETTINGS____________*/
+void DRV8711_init(DRV8711_ENGINE_t* obj){
+    if(nullptr== obj) return;
+    obj->ctrl   = 0xC10;
+    obj->torque = 0x1FF;
+    obj->off    = 0x030;
+    obj->blank  = 0x080;
+    obj->decay  = 0x110;
+    obj->stall  = 0x040;
+    obj->drive  = 0xA59;
+}
+void DRV8711_reset(DRV8711_ENGINE_t* obj){
+    if(nullptr == obj) return;
+    obj->ctrl   = 0xC10;
+    obj->torque = 0x1FF;
+    obj->off    = 0x030;
+    obj->blank  = 0x080;
+    obj->decay  = 0x110;
+    obj->stall  = 0x040;
+    obj->drive  = 0xA59;
 
-
+}
 #endif /* INC_DRV8711_ENGINE_H_ */
--- Core/Inc/DRV8711/error.h
+++ Core/Inc/DRV8711/error.h
@@ -5,10 +5,18 @@
 #ifndef INC_DRV8711_ERROR_H_
 #define INC_DRV8711_ERROR_H_
 typedef enum{
-	NO_ERROR = 0x0000,
-	ENGINE_STALL = 0xFFFF,
+	NO_ERROR = 0xFFFF,
 	TIMEOUT = 0xDEAF,
 	UNKNOWN = 0xDEAD,
+/*_______________HARDWARE FAULTS_______________*/
+  OTS = 0,  	// Overtemperature shutdown
+  AOCP = 1, 	// Overcurrent shutdown
+  BOCP = 2, 	// Channel B overcurrent shutdown
+  APDF = 3,		// Channel A predriver fault
+  BPDF = 4,		// Channel B predriver fault
+  UVLO = 5,		// Undervoltage lockout
+  STD = 6,		// Stall detected
+  STDLAT = 7,	// Latched stall detect
 }DRV8711_ERROR_t;
 
 DRV8711_ERROR_t DRV8711_error_handler();
